6af9e902c838debeb49d40a78c74fd6c33e1cecd
YutaroM <yutaroapp@gmail.com>
2020-12-22 00:13:25 +0900

    #1 ルーティングの修正。version8くらいから書き方が異なるよう。

--- Laravel/routes/web.php
+++ Laravel/routes/web.php
@@ -1,9 +1,10 @@
 <?php
 
+use App\Http\Controllers\TaskController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+Route::get('/folders/{id}/tasks', [TaskController::class, 'index']);
