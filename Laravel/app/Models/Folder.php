444faf1c45864dfb42ef4b8967eda21151b06132
YutaroM <yutaroapp@gmail.com>
2020-12-25 22:59:02 +0900

    Folderモデルクラスの追加

---
+++ Laravel/app/Models/Folder.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Folder extends Model
+{
+    // https://readouble.com/laravel/8.x/ja/database-testing.html
+    // use HasFactory;
+}
