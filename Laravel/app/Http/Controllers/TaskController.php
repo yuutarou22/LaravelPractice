c0eaf353d3f4ab917332467d11626033836734f6
YutaroM <yutaroapp@gmail.com>
2020-12-28 22:23:55 +0900

    TaskControllerの修正

--- Laravel/app/Http/Controllers/TaskController.php
+++ Laravel/app/Http/Controllers/TaskController.php
@@ -2,11 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Folder;
 use Illuminate\Http\Request;
 
 class TaskController extends Controller
 {
     public function index() {
-        return "Hello World";
+
+        // 全てのフォルダデータをDBから取得
+        $folders = Folder::all();
+
+        // view関数を使って、テンプレートに対してデータを渡し、その結果を返却（出力）する
+        return view('tasks/index', [
+            'folders' => $folders,
+        ]);
+
+
+        print("hollo");
+
     }
 }
